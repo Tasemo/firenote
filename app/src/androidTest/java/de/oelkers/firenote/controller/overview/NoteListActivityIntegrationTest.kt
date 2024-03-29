c5e73434b6eafd09731c358a202bf222b29668ef
Tim Oelkers <tim.oelkers@web.de>
2024-03-29 11:36:04 +0100

    Add integration test for search

--- app/src/androidTest/java/de/oelkers/firenote/controller/overview/NoteListActivityIntegrationTest.kt
+++ app/src/androidTest/java/de/oelkers/firenote/controller/overview/NoteListActivityIntegrationTest.kt
@@ -3,13 +3,11 @@ package de.oelkers.firenote.controller.overview
 import androidx.activity.viewModels
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.longClick
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import de.oelkers.firenote.R
@@ -19,6 +17,8 @@ import de.oelkers.firenote.controllers.overview.NoteViewModel
 import de.oelkers.firenote.controllers.overview.NoteViewModelFactory
 import de.oelkers.firenote.models.Note
 import de.oelkers.firenote.persistence.NoteRepository
+import de.oelkers.firenote.testing.atPosition
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -52,7 +52,7 @@ class NoteListActivityIntegrationTest {
     }
 
     @Test
-    fun testItemsAreDeletedOnQuickDelete() {
+    fun testThatItemsAreDeletedOnQuickDelete() {
         val repository = NoteRepository(InstrumentationRegistry.getInstrumentation().targetContext)
         val notes = listOf(Note("Note1"), Note("Note2"))
         repository.saveAllNotes(notes)
@@ -64,6 +64,25 @@ class NoteListActivityIntegrationTest {
             it.onActivity { activity ->
                 val viewModel: NoteViewModel by activity.viewModels { NoteViewModelFactory(repository) }
                 assertTrue(viewModel.allNotes.isEmpty())
+            }
+        }
+    }
+
+    @Test
+    fun testThatItemsAreFilteredOnSearch() {
+        val repository = NoteRepository(InstrumentationRegistry.getInstrumentation().targetContext)
+        val notes = mutableListOf(Note("Note1", title = "TestTitle"), Note("Note2"), Note("Note3", content = "ContentTest"))
+        repository.saveAllNotes(notes)
+        launchActivity<NoteListActivity>().use {
+            onView(withId(R.id.search_button)).perform(click())
+            onView(withId(androidx.appcompat.R.id.search_src_text)).perform(typeText("Test"))
+            onView(withId(R.id.notesView)).check(matches(atPosition(0, hasDescendant(withText("TestTitle")))))
+            onView(withId(R.id.notesView)).check(matches(atPosition(1, hasDescendant(withText("ContentTest")))))
+            it.onActivity { activity ->
+                val viewModel: NoteViewModel by activity.viewModels { NoteViewModelFactory(repository) }
+                assertEquals(notes, viewModel.allNotes)
+                notes.removeAt(1)
+                assertEquals(notes, viewModel.filteredNotes)
             }
         }
     }
